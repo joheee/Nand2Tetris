91096785c5125ee392a096f2708866a7de5d7add
joheee <94522173+joheee@users.noreply.github.com>
2023-03-13 17:16:07 +0700

    project 4 done

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -1,14 +1,47 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+(BEGIN)
+	@keyboard
+	M=D
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+	@24576
+	D=A
+(CHECK_KEYBOARD)
+	@current
+	M=D
 
-// Put your code here.
+	@keyboard
+	A=M
+	D=M
+	
+	@24575
+	D=A
+	
+	@DRAW
+	D;JNE
+	
+	@fillvalue
+	M=0
+	
+	@fillvalue
+	M=-1
+(DRAW)
+	@current
+	A=M
+	M=D
+
+	@fillvalue
+	D=M
+
+	@current
+	D=M
+
+	@16384
+	D=D-A
+
+	@CHECK_KEYBOARD
+	D;JLE
+
+	@current
+	M=M-1
+
+	@DRAW
+	0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -1,12 +1,24 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
+(BEGIN)
+	@R2
+	M=0
+(LOOP)
+	@R1
+	D=M
 
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-//
-// This program only needs to handle arguments that satisfy
-// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+	@END
+	D;JLE
+	
+	@R0
+	D=M
+	
+	@R1
+	M=M-1
 
-// Put your code here.
+	@R2
+	M=M+D
+	
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
